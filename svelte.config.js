7c150f56cb7e4ebc4ebc910d5844d539f91c39b0
iris8721 <flexion360@gmail.com>
2026-09-15 17:58:52 -0400

    Add a Content Security Policy

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,19 @@ import adapter from '@sveltejs/adapter-node';
 const config = {
 	kit: {
 		adapter: adapter(),
+		csp: {
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self'],
+				'style-src': ['self', 'unsafe-inline', 'https://fonts.googleapis.com'],
+				'font-src': ['self', 'https://fonts.gstatic.com'],
+				'img-src': ['self', 'data:', 'blob:', 'https://cdn.cloudflare.steamstatic.com'],
+				'connect-src': ['self'],
+				'frame-ancestors': ['none'],
+				'base-uri': ['self'],
+				'form-action': ['self'],
+			},
+		},
 	},
 };
 
